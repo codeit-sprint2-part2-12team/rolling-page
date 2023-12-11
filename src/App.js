d90bcdbceeae582ad9380608f4d6753666244a5e
Sungeunji <danceunji@gmail.com>
2023-12-11 13:58:01 +0900

    style: SecondaryButton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import GlobalStyle from './GlobalStyle';
+import SecondaryButton from './components/SecondaryButton';
 
 function App() {
+  
   return (
     <div className="App">
       <GlobalStyle />
+      <SecondaryButton>Button</SecondaryButton>
+      <SecondaryButton disabled>Disabled</SecondaryButton>
     </div>
   );
 }
