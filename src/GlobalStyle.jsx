a598a7a76877faecfd836626e58e952f6bafdc45
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-11 10:40:07 +0900

    style: GlobalStyle 적용

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+ font-size: 62.5%;
+}
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+`;
+
+export default GlobalStyle;
